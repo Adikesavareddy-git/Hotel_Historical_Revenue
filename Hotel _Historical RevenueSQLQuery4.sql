d8fc5f675fc04f4fcdee0c130f849316dbc9ffc1
ADIKESAVA REDDY KATTERAPALLI <reddyadikesava07@gmail.com>
2025-12-30 18:27:05 +0530

    Add files via upload

---
+++ Hotel _Historical RevenueSQLQuery4.sql
@@ -0,0 +1,12 @@
+with hotels as(
+Select * from dbo.['2018$']
+union
+Select * from dbo.['2019$']
+union
+Select * from dbo.['2020$']
+) 
+Select * from hotels
+left join dbo.market_segment$
+on hotels.market_segment = market_segment$.market_segment
+left join dbo.meal_cost$
+on dbo.meal_cost$.meal = hotels.meal
